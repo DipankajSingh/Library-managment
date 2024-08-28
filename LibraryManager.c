e2dbe0ef3c7e41f19bb9135f5cab3663809685ec
Dipankaj Singh <dipankajsingh2@.com>
2024-08-29 00:57:56 +0530

    fixed some bugs

--- LibraryManager.c
+++ LibraryManager.c
@@ -30,39 +30,34 @@ void add_book(Book book)
         books[next_book_index].available = book.available;
         next_book_index++;
 
-        printf("Book added successfully!\n");
+        printf("Book added successfully!\n\n");
     }
     else
     {
-        printf("Library is full!\n");
+        printf("Library is full!\n\n");
     }
 }
 
 int find_book_index(char name[MAX_SIZE])
 {
+    printf("The query string is: %s\n\n", name);
     for (int i = 0; i < next_book_index; i++)
     {
-        if (strcmp(books[i].name, name) != 0)
+        if (strcmp(books[i].name, name) == 0)
         {
+            printf("The query string true\n\n");
+
             return i;
         }
-        else
-        {
-            return -1;
-        }
     }
+    printf("The query string false\n\n");
+    return -1;
 }
 
 int main()
 {
     int option;
     Book book;
-    printf("Choose an option:\n");
-    printf("1. Add a book\n");
-    printf("2. Remove a book\n");
-    printf("3. Update a book\n");
-    printf("4. Display all books\n");
-    printf("5. Exit\n");
 
     int true = 1;
     int false = 0;
@@ -71,52 +66,65 @@ int main()
 
     while (library_open == true)
     {
+        printf("Choose an option:\n");
+        printf("1. Add a book\n2. Remove a book\n3. Update a book\n4. Display all books\n5. Exit\n\n");
         scanf("%d", &option);
 
         switch (option)
         {
         case 1:
-            printf("Enter the name of the book: ");
+            printf("Name: ");
             scanf("%s", book.name);
-            printf("Enter the author of the book: ");
+            printf("Author: ");
             scanf("%s", book.author);
-            printf("Enter the price of the book: ");
+            printf("Price: ");
             scanf("%d", &book.price);
-            printf("Enter the availability of the book: ");
+            printf("Availability: ");
             scanf("%d", &book.available);
             add_book(book);
             break;
         case 2:
+            if (next_book_index == 0)
+            {
+                printf("Nothing in Library!\n\n");
+                break;
+            };
             printf("Enter the name of the book to remove: ");
             char RemoveName[MAX_SIZE];
             scanf("%s", RemoveName);
             int index = find_book_index(RemoveName);
-
+            printf("The book Position is: %d\n\n", index);
             if (index != -1)
             {
                 for (int i = index; i < next_book_index - 1; i++)
                 {
                     books[i] = books[i + 1];
                 };
-                printf("book removed!");
+                printf("book removed!\n\n");
             };
             break;
         case 3:
             // update a book
             break;
         case 4:
+            if (next_book_index == 0)
+            {
+                printf("Nothing in Library!\n\n");
+                break;
+            };
+
             for (int i = 0; i < next_book_index; i++)
             {
-                printf("Book Name: %s", books[i].name);
-                break;
-            case 5:
-                library_open = false;
-                break;
-            default:
-                printf("Invalid option\n");
-                break;
-            }
+                printf("Book Name: %s\nBook Author: %s\nBook Price: %d$\nBook Available: %dpic\n\n", books[i].name, books[i].author, books[i].price, books[i].available);
+            };
+            break;
+        case 5:
+            library_open = false;
+            break;
+        default:
+            printf("Invalid option\n\n");
+            break;
         }
     }
     return 0;
-}
+};
